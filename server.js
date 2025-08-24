75c09001d8f0ded812dba9e434d0bdebb2bfd242
Uttkarsh <uttkarshpahuja@gmail.com>
2025-08-25 01:11:03 +0530

    Logic update

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs')
 
 app.get("/",(req,res)=>{
-    res.render("index");
+    res.render("index",{bmi: null});
+})
+
+app.post("/calculate",(req,res)=>{
+    const floatlength = (req.body.length);
+    const floatweight = (req.body.mass);
+    const bmi = floatweight/((floatlength/100)*(floatlength/100));
+    res.render("index",{bmi: bmi.toFixed(2)});
+
 })
 
 app.listen('3000',()=>{
